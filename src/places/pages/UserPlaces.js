3f890cfdb24ca4b62f99b895436fdff911030036
RAO YANG <lightrao@hotmail.com>
2022-12-23 16:02:55 +0800

    use .env to store api key

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -12,8 +12,8 @@ const DUMMY_PLACES = [
       "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fphoto.tuchong.com%2F34176%2Ff%2F4099742.jpg&refer=http%3A%2F%2Fphoto.tuchong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1673751815&t=4a3a631b7af609cdb0f79be6f6795de6",
     address: "20 W 34th St., New York, NY 10001",
     location: {
-      lat: 40.3704051,
-      lng: -104.5743473,
+      lat: 40.748817,
+      lng: -73.985428,
     },
     creator: "u1",
   },
@@ -25,8 +25,8 @@ const DUMMY_PLACES = [
       "https://en.wikipedia.org/wiki/Empire_State_Building#/media/File:Empire_State_Building_(aerial_view).jpg",
     address: "20 W 34th St., New York, NY 10001",
     location: {
-      lat: 40.3704051,
-      lng: -104.5743473,
+      lat: 40.748817,
+      lng: -73.985428,
     },
     creator: "u2",
   },
--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,17 +1,26 @@
 import React, { useRef, useEffect } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
 
 import "./Map.css";
 
 const Map = (props) => {
   const mapRef = useRef();
   const { center, zoom } = props;
+
   useEffect(() => {
-    const map = new window.google.maps.Map(mapRef.current, {
-      center: center,
-      zoom: zoom,
+    const loader = new Loader({
+      apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+      version: "weekly",
     });
 
-    new window.google.maps.Marker({ position: center, map: map });
+    loader.load().then(() => {
+      const map = new window.google.maps.Map(mapRef.current, {
+        center: center,
+        zoom: zoom,
+      });
+
+      new window.google.maps.Marker({ position: center, map: map });
+    });
   }, [center, zoom]);
 
   return (
