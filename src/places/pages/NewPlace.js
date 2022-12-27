5d15a5224436c66bece228fc82ecb5f4a22f0532
RAO YANG <lightrao@hotmail.com>
2022-12-27 20:57:34 +0800

    Finishing the "Add Place" Form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -49,8 +49,13 @@ const NewPlace = () => {
     [dispatch]
   );
 
+  const placeSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(fromState.inputs); // send this to backend!
+  };
+
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={placeSubmitHandler}>
       <Input
         id="title"
         element="input"
@@ -66,6 +71,14 @@ const NewPlace = () => {
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (at least 5 characters)."
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address."
         onInput={inputHandler}
       />
       <Button type="submit" disabled={!fromState.isValid}>
