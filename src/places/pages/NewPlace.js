717d864ad8603ffcc03302cc9756a412424f871f
RAO YANG <lightrao@hotmail.com>
2023-01-04 08:00:26 +0800

    Creating a Custom Form Hook (useForm)

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useReducer } from "react";
+import React from "react";
 
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
@@ -6,53 +6,22 @@ import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH,
 } from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
 import "./PlaceForm.css";
 
-const formReducer = (state, action) => {
-  switch (action.type) {
-    case "INPUT_CHANGE":
-      let formIsValid = true;
-      for (const inputId in state.inputs) {
-        if (inputId === action.inputId) {
-          formIsValid = formIsValid && action.isValid;
-        } else {
-          formIsValid = formIsValid && state.inputs[inputId].isValid;
-        }
-      }
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.inputId]: { value: action.value, isValid: action.isValid },
-        },
-        isValid: formIsValid,
-      };
-
-    default:
-      return state;
-  }
-};
-
 const NewPlace = () => {
-  const [fromState, dispatch] = useReducer(formReducer, {
-    inputs: {
-      title: { value: "", isValid: false },
-      description: { value: "", isValid: false },
-      address: { value: "", isValid: false },
+  const [formState, inputHandler] = useForm(
+    {
+      title: { value: "", isValid: "" },
+      description: { value: "", isValid: "" },
+      address: { value: "", isValid: "" },
     },
-    isValid: false,
-  });
-
-  const inputHandler = useCallback(
-    (id, value, isValid) => {
-      dispatch({ type: "INPUT_CHANGE", value, isValid, inputId: id });
-    },
-    [dispatch]
+    false
   );
 
   const placeSubmitHandler = (event) => {
     event.preventDefault();
-    console.log(fromState.inputs); // send this to backend!
+    console.log(formState.inputs); // send this to backend!
   };
 
   return (
@@ -82,7 +51,7 @@ const NewPlace = () => {
         errorText="Please enter a valid address."
         onInput={inputHandler}
       />
-      <Button type="submit" disabled={!fromState.isValid}>
+      <Button type="submit" disabled={!formState.isValid}>
         ADD PLACE
       </Button>
     </form>
