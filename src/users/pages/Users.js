7c941c6332179aff35ae4eeaa11a003d9e329cde
RAO YANG <lightrao@hotmail.com>
2023-01-31 08:22:42 +0800

    Getting Users with a GET Request

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import UsersList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 // stateful component
 const Users = () => {
-  // dummy data
-  const USERS = [
-    {
-      id: "u1",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/1920px-FullMoon2010.jpg",
-      name: "Neo",
-      places: 1,
-    },
-    {
-      id: "u2",
-      image: "imge url2",
-      name: "Max",
-      places: 4,
-    },
-  ];
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
 
-  return <UsersList items={USERS} />;
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/users");
+        const responsData = await response.json();
+
+        if (!response.ok) {
+          return new Error(responsData.message);
+        }
+
+        setLoadedUsers(responsData.users);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </>
+  );
 };
 
 export default Users;
