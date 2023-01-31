fbb56ea9644ec88c4fcd76854293ad40ebdb9bec
light <raoyangkm@gmail.com>
2023-01-31 17:47:20 +0800

    Improving the Custom Http Hook

--- src/shared/hooks/http-hook.js
+++ src/shared/hooks/http-hook.js
@@ -25,14 +25,22 @@ export const useHttpClient = () => {
         });
 
         const responseData = await response.json();
+
+        // make sure we don't try to cancel our request, which already completed
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (reqCtrl) => reqCtrl !== httpAbortCtrl
+        );
+
         if (!response.ok) {
           throw new Error(responseData.message);
         }
+        setIsLoading(false);
         return responseData;
       } catch (err) {
         setError(err.message);
+        setIsLoading(false);
+        throw err;
       }
-      setIsLoading(false);
     },
     []
   );
