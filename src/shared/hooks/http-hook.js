06cc6ca464f4670cb8714571d00b0729fec0df6f
light <raoyangkm@gmail.com>
2023-01-31 11:28:07 +0800

    Creating a Custom Http Hook

---
+++ src/shared/hooks/http-hook.js
@@ -0,0 +1,51 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // A reference here in the end is just a piece of data
+  // which will not change when this function(useHttpClient) runs again
+  // useHttpClient run again whenever the component which uses this hook rerenders
+  // this will basically store data across renderer cycles
+  const activeHttpRequests = useRef([]);
+
+  // The function never gets recreated when the component that uses this hook renders
+  const sendRequest = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setIsLoading(true);
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbortCtrl.signal,
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        return responseData;
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    },
+    []
+  );
+
+  const clearError = useCallback(() => {
+    setError(null);
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+    };
+  }, []);
+
+  return { isLoading, error, sendRequest, clearError };
+};
